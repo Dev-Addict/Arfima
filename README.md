15f77bf91aa0d66981ace419d047d391fec5b597
Aria AzadiPour <aazadipour@pm.me>
2025-06-03 12:49:16 -0230

    show a list of files in the directory

---
+++ README.md
@@ -0,0 +1,11 @@
+# Arfima
+
+A file manager cli tool.
+
+## License
+
+Copyright (c) Aria AzadiPour <aazadipour@pm.me>
+
+This project is licensed under the MIT license ([LICENSE] or <http://opensource.org/licenses/MIT>)
+
+[LICENSE]: ./LICENSE
---
+++ src/main.rs
@@ -0,0 +1,33 @@
+mod app;
+
+use std::{env, path::PathBuf};
+
+use crate::app::App;
+
+fn main() -> color_eyre::Result<()> {
+    let directory = match env::args().nth(1) {
+        Some(p) => {
+            let path = PathBuf::from(p).canonicalize().unwrap_or_else(|_| {
+                eprintln!("Invalid path provided.");
+                std::process::exit(1);
+            });
+
+            if path.is_dir() {
+                path
+            } else {
+                eprintln!("The path provided needs to be a directory");
+                std::process::exit(1);
+            }
+        }
+        None => env::current_dir().expect("Failed to get current directory."),
+    };
+
+    color_eyre::install()?;
+
+    let terminal = ratatui::init();
+    let result = App::new(directory.to_string_lossy().to_string()).run(terminal);
+
+    ratatui::restore();
+
+    result
+}
