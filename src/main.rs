6266c56f8eb29ae643bcda01d2b2b43920e3c7c8
Aria AzadiPour <aria.azadi.pour@protonmail.com>
2025-09-04 12:05:47 -0230

    logging added

--- src/main.rs
+++ src/main.rs
@@ -11,6 +11,8 @@ mod utils;
 use std::{env, path::PathBuf};
 
 use directories::ProjectDirs;
+use log::{error, info};
+use stderrlog::LogLevelNum;
 
 use crate::{
     app::App,
@@ -19,38 +21,55 @@ use crate::{
 };
 
 fn main() -> color_eyre::Result<()> {
+    stderrlog::new()
+        .module(module_path!())
+        .quiet(false)
+        .verbosity(LogLevelNum::Info)
+        .init()
+        .unwrap();
+
     let directory = match env::args().nth(1) {
         Some(p) => {
             let path = PathBuf::from(p).canonicalize().unwrap_or_else(|_| {
-                eprintln!("Invalid path provided.");
+                error!("Invalid path provided.");
                 std::process::exit(1);
             });
 
             if path.is_dir() {
                 path
             } else {
-                eprintln!("The path provided needs to be a directory");
+                error!("The path provided needs to be a directory");
                 std::process::exit(1);
             }
         }
-        None => env::current_dir().expect("Failed to get current directory."),
+        None => env::current_dir().unwrap_or_else(|_| {
+            error!("Failed to get current directory.");
+            std::process::exit(1);
+        }),
     };
 
-    let config_file = if let Some(proj_dirs) = ProjectDirs::from(
+    let config_file = match ProjectDirs::from(
         package::get().tld(),
         package::get().domain(),
         package::get().application(),
     ) {
-        proj_dirs.config_dir().to_owned().join(CONFIG_FILE_NAME)
-    } else {
-        eprintln!("Failed to get project directories");
-        std::process::exit(1);
+        Some(proj_dirs) => proj_dirs.config_dir().to_owned().join(CONFIG_FILE_NAME),
+        None => {
+            error!(
+                "Failed to get project directories. Please report this issue in GitHub: https://github.com/Dev-Addict/Arfima"
+            );
+            std::process::exit(1);
+        }
     };
 
     color_eyre::install()?;
 
-    let config = Config::try_from(config_file).unwrap_or_else(|_e| {
-        // TODO: Add logging for the error
+    let config = Config::try_from(config_file.clone()).unwrap_or_else(|e| {
+        error!(
+            "Failed to get config from file({}): {e}",
+            config_file.to_string_lossy()
+        );
+        info!("Using the default config.");
         Config::default()
     });
 
